2abbaa135aab5ab2ce3b5bd34dc15d2bae924937
Lucas Gabriel Henschel <lucas101henschel@gmail.com>
2023-10-13 12:06:12 -0300

    Inserção de usuário com POST

--- src/main/java/com/lucasweb/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasweb/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasweb.workshopmongo.domain.User;
+import com.lucasweb.workshopmongo.dto.UserDTO;
 import com.lucasweb.workshopmongo.repository.UserRepository;
 import com.lucasweb.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 
 }
